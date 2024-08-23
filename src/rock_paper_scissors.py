562892011982ef481f0181f77f59bea4b3058f16
lokashokavinashini <123806240+lokashokavinashini@users.noreply.github.com>
2024-08-23 15:28:10 +0530

    Update rock_paper_scissors.py

--- src/rock_paper_scissors.py
+++ src/rock_paper_scissors.py
@@ -4,13 +4,22 @@
 def get_user_choice():
     """To get user's choice"""
     user_choice = input("Which do you choose: Rock, Paper, Scissors?")
+    return user_choice
 
 
 def get_computer_choice():
     """To get computer's choice"""
-    pass
+    return None
 
 
-def determine_winner():
+def determine_winner(user_choice, computer_choice):
     """Determine who is the winner and display the result"""
-    pass
+    print(user_choice)
+    print(computer_choice)
+    return None
+
+
+user_choice = get_user_choice()
+computer_choice = get_computer_choice()
+result = determine_winner(user_choice, computer_choice)
+print(result)

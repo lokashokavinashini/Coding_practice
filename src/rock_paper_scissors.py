d45c99e47ac1d76818c5659476eb57205fe901c4
lokashokavinashini <123806240+lokashokavinashini@users.noreply.github.com>
2024-08-23 15:19:59 +0530

    Update rock_paper_scissors.py

--- src/rock_paper_scissors.py
+++ src/rock_paper_scissors.py
@@ -1,12 +1,15 @@
 """ A script for playing Rock, Paper, Scissors with Computer """
 
+
 def get_user_choice():
     """ To get user's choice"""
     user_choice = input("Which do you choose: Rock, Paper, Scissors?")
 
+
 def get_computer_choice():
     """ To get computer's choice"""
     pass
+
 
 def determine_winner():
     """ Determine who is the winner and display the result """
